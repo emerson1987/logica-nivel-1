7427ce3ea848c8f0ae0f508db4b798bd2fe8b70d
emerson1987 <emersoncosme708@gmail.com>
2025-04-08 21:56:50 -0300

    Update ex-02.py

--- ex-02.py
+++ ex-02.py
@@ -1,4 +1,4 @@
-2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
 print("emerson da costa")
 print("rio de janeiro")
 print("rio de janeiro")
